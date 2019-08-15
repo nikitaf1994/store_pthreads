e2800e366e30a06753b4d8b6bf4148a4fa66c55d
Nikita Filipenko <nikitaaaaa1994@mail.ru>
2019-08-15 16:36:42 +0700

    All work fine

--- main.c
+++ main.c
@@ -2,103 +2,82 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <time.h>
 
 #define ERROR_CREATE_THREAD -11
 #define ERROR_JOIN_THREAD   -12
 #define SUCCESS		0
 
-int store[9];
-int buyer[5];
+int store[4];
 int q=0;
-int w=0;
-int z=0;
+pthread_mutex_t lock;
+int counter=0;
 
-void *buying() 
+void *buying()
 {
-	while(buyer[0]>0 || buyer[1]>0 || buyer[2]>0)
+	srand(time(NULL));
+        int buyer=rand()%(11000+1-9000)+9000;
+	while(buyer>0 && counter<3)
 	{
-		q=0;
-		if(buyer[w]>0 && buyer[w+3]==0)
+		srand(time(NULL));
+        	q=rand()%(4+1-0)+0;//random store
+		if(store[q]>0)
 		{
-			buyer[w+3]=1;
-			while (q<5)
-			{
-				if(store[q]>0 && store[q+5]==0)
-				{
-					store[q+5]=1;
-					buyer[w]=buyer[w]-store[q];
-					printf("Buyer %d bought from store %d %d products and now he have %d money\n", w,q,store[q],buyer[w]);
-					store[q]=0;
-					store[q+5]=0;
-					sleep(3);
-				}
-				else 
-				{
-					q++;
-				}
-			}
-			buyer[w+3]=0;
-		}//if buyer>0
-		else if(w<3)
-		{
-			w++;
-		}
-		else
-		{
-			w=0;
+			pthread_mutex_lock(&lock);
+			buyer=buyer-store[q];
+			printf("Buyer bought from store %d %d products and now he have %d money\n", q,store[q],buyer);
+			store[q]=0;
+			pthread_mutex_unlock(&lock);
+			sleep(3);
 		}
 	}
-	printf("I'm done!\n");
+	printf("1 thread done!\n");
+	counter++;
+	printf("Counter=%d\n",counter);
 	return SUCCESS;
 }
 
 void *give()
 {
-	while(buyer[0]>0 || buyer[1]>0 || buyer[2]>0)
+	while(counter<3)
 	{
-		if(store[z]<=0 && store[z+5]==0)
+		srand(time(NULL));
+                q=rand()%(4+1-0)+0;//random store
+		if(store[q]<=0)
 		{
-			store[z+5]=1;
-                        store[z]=store[z]+50000;
-                        printf("Added 50000 for store %d\n",z);
-                        store[z+5]=0;
+			pthread_mutex_lock(&lock);
+                        store[q]=store[q]+500;
+                        printf("Added 500 for store %d\n",q);
+			pthread_mutex_unlock(&lock);
                         sleep(2);
-		}
-		else if (z<5)
-		{
-			z++;
-		}
-		else
-		{
-			z=0;
 		}
 	}
 }
 
 int main()
 {
-	int i,j,k;
+	int i;
+	int j;
 	pthread_t threads[3];
 	int status;
 	int status_addr;
-	int counter =0;
+	srand(time(NULL));
 	for(j=0;j<5;j++)
 	{
 		store[j]=rand()%(1100+1-900)+900;
 		printf("store %d = %d\n", j,store[j]);
 	}
 
-	for(k=0;k<3;k++)
-        {
-        	buyer[k]=rand()%(11000+1-9000)+9000;
-        	printf("buyer %d = %d\n", k, buyer[k]);
-        }
-	
-	for (i = 0; i < 3; i++) 
+	if (pthread_mutex_init(&lock, NULL) != 0)
+    	{
+        	printf("\n mutex init failed\n");
+        	return 1;
+    	}
+
+	for (i = 0; i < 3; i++)
 	{
-//status = pthread_create(&threads[i], NULL, buying, (void*) numbers);
 		status = pthread_create(&threads[i], NULL, buying, NULL);
-        	if (status != 0) 
+        	if (status != 0)
 		{
             		printf("main error: can't create thread, status = %d\n", status);
             		exit(ERROR_CREATE_THREAD);
@@ -106,31 +85,23 @@ int main()
     	}
 
 	status = pthread_create(&threads[3], NULL, give, NULL);
-                if (status != 0) 
+                if (status != 0)
                 {
                         printf("main error: can't create thread, status = %d\n", status);
                         exit(ERROR_CREATE_THREAD);
                 }
 
 
-	for (i = 0; i < 3; i++) 
+	for (i = 0; i < 4; i++)
 	{
         	status = pthread_join(threads[i], (void**)&status_addr);
-        	if (status != SUCCESS) 
+        	if (status != SUCCESS)
 		{
             		printf("main error: can't join thread, status = %d\n", status);
             		exit(ERROR_JOIN_THREAD);
         	}
         	printf("joined with address %d\n", status_addr);
     	}
-
-	status = pthread_join(threads[3], (void**)&status_addr);
-                if (status != SUCCESS) 
-                {
-                        printf("main error: can't join thread, status = %d\n",status);
-                        exit(ERROR_JOIN_THREAD);
-                }
-                printf("joined with address %d\n", status_addr);
 
 	return 0;
 }
